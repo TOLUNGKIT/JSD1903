2ee838f6f63710083f6194f73a0b8ef5e55e8e14
TOLUNGKIT <44824624+TOLUNGKIT@users.noreply.github.com>
2019-03-29 03:34:42 +0800

    Update MultiTable.java

--- src/day0328/MultiTable.java
+++ src/day0328/MultiTable.java
@@ -5,7 +5,7 @@ public class MultiTable {
 	public static void main(String[] args) {
 		int num = 9;
 		for(int i =1;i<=num;i++) {
-			System.out.print(i+"*"+num+"="+i*num+"\t");// "\t"-----char 空八格  你回家  斗罗大陆的领导
+			System.out.print(i+"*"+num+"="+i*num+"\t");// "\t"-----char 空八格  
 		}
 		System.out.println();//换行
 	}
