ecaaa6156d77e790bce987d7609dfcc463ad8913
TOLUNGKIT <44824624+TOLUNGKIT@users.noreply.github.com>
2019-03-29 03:21:14 +0800

    Update MultiTable.java

--- src/day0328/MultiTable.java
+++ src/day0328/MultiTable.java
@@ -5,9 +5,9 @@ public class MultiTable {
 	public static void main(String[] args) {
 		int num = 9;
 		for(int i =1;i<=num;i++) {
-			System.out.print(i+"*"+num+"="+i*num+"\t");
+			System.out.print(i+"*"+num+"="+i*num+"\t");// "\t"-----char 空八格
 		}
-		System.out.println();
+		System.out.println();//换行
 	}
 
 }
