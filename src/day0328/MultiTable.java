dd243e73938b79b973afa324ead43d3ee0b26a34
TOLUNGKIT <44824624+TOLUNGKIT@users.noreply.github.com>
2019-03-29 03:19:27 +0800

    Update MultiTable.java

--- src/day0328/MultiTable.java
+++ src/day0328/MultiTable.java
@@ -3,8 +3,11 @@ package day0328;
 public class MultiTable {
 
 	public static void main(String[] args) {
-		
-
+		int num = 9;
+		for(int i =1;i<=num;i++) {
+			System.out.print(i+"*"+num+"="+i*num+"\t");
+		}
+		System.out.println();
 	}
 
 }
