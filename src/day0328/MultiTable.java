d87dfa739e17649cdb77bccb4face7c9a331d2b9
TOLUNGKIT <44824624+TOLUNGKIT@users.noreply.github.com>
2019-03-30 23:02:13 +0800

    Update MultiTable.java

--- src/day0328/MultiTable.java
+++ src/day0328/MultiTable.java
@@ -3,11 +3,15 @@ package day0328;
 public class MultiTable {
 
 	public static void main(String[] args) {
-		int num = 9;
-		for(int i =1;i<=num;i++) {
-			System.out.print(i+"*"+num+"="+i*num+"\t");// "\t"-----char 空八格  
+
+		for(int num = 1;num<=9;num++) {//控制行
+			for(int i =1;i<=num;i++) {//控制列 
+				System.out.print(i+"*"+num+"="+i*num+"\t");// "\t"-----char 空八格  
+			}
+			System.out.println();//换行  
 		}
-		System.out.println();//换行  
+		
+		
 	}
 
 }
